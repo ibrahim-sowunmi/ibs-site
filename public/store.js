65539c247a54e4f5fc055889108614e4eb2a73fb
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2022-01-21 17:06:01 +0000

    Functional navbar, desktop and mobile

---
+++ public/store.js
@@ -0,0 +1,87 @@
+{/* <main class="container py-24 px-60 bg-gray-100">
+    <div className="content">
+        <img className="profile-pic" alt="Ibrahim Sowunmi" src={img} />
+        <p>
+          I'm currently a 24 year old Solution Engineer based in based in{' '}
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Dublin"
+          >
+            {' '}
+            Dublin
+          </a>
+          .<br>
+          <br>
+          Prior to that I completed a BSc in{' '}
+          <span className="highlight">
+            <b>Computer Science with Human-computer Interaction</b>
+          </span>{' '}
+          @
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://www.manchester.ac.uk/"
+          >
+            {' '}
+            The University of Manchester
+          </a>
+          .<br>
+          Passionate about problem-solving and intuitive design.
+        </p>
+        <p>
+          <br>
+          <b>What’s the point of this site?</b>
+          <br>
+          <br>I started this to create a bit of a digital presence (No
+          socials bar Linkedin). I’ve been meaning to write about random things
+          for a while, and I got tired of my ideas/thoughts stacking up in my
+          brain and gradually fading into nothing.<br>
+          Also, writing is just a good skill to have! I don't write much as I've
+          been on the STEM fast track, and during Uni all the writing I did felt
+          like typing with my toes. This site is an attempt to develop that part
+          of my brain a bit (not the toe typing part).
+          <br>
+          <br>
+          <b>About me</b>
+          <br>
+          <br>
+          <p>
+            I enjoy travelling, keeping fit (
+            <i>Boxing, Running, Calisthenics, Gym</i>
+            ), philosophizing, learning and thinking about things like
+            geopolitics, finance, crypto, urban/environmental sustainability,
+            digital art, photography, software engineering, neuroscience,
+            economics, technology, etc. When I'm not doing something related to
+            those, I'm likely working through my{' '}
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.goodreads.com/review/list/91646908-ibrahim-sowunmi?ref=nav_mybooks&shelf=to-read"
+            >
+              reading list.
+            </a>
+          </p>
+        </p>
+        <br>
+        <br>
+        <p>Website is built by me and works via S3, Route53 & React... WIP</p>
+  
+        <br>
+        <br>
+        <p>
+          View my&nbsp;
+          <a href="https://www.linkedin.com/in/ibrahim-sowunmi/?originalSubdomain=uk">
+            LinkedIn
+          </a>
+          ,&nbsp;
+          <a href="https://trailblazer.me/id/isowunmi">Trailhead</a>
+          ,&nbsp;
+          <a href="https://open.spotify.com/playlist/2SeQ77fxp8j82bTNb7AJ91?si=aa70e218a8f74f06">
+            Spotify
+          </a>
+          &nbsp;or&nbsp;
+          <a href="https://github.com/ibrahim-sowunmi">Github</a>
+        </p>
+      </div>
+</main> */}
